0820b4ccf417fba2a33792d8ecda6cb665a33548
Diego Lucana <u20201c122@upc.edu.pe>
2025-11-19 10:07:30 -0500

    url

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
+  import.meta.env.VITE_API_BASE_URL ||   "https://robots-musician-buildings-polyester.trycloudflare.com/api/v1";
+
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
